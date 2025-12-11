ee128f322686890d0ee156e652bdfdc432f5870d
chenkianwee <chenkianwee@gmail.com>
2025-12-11 17:37:20 -0500

    0.0.1 version

--- raytracemrt_webapp/main.py
+++ raytracemrt_webapp/main.py
@@ -1,95 +1,338 @@
-import io
-import math
-import time
 import asyncio
-
-# from js import document, Uint8Array, File, URL
+import numpy as np
+from time import perf_counter
 
 from pyodide.ffi.wrappers import add_event_listener
-from pyodide.ffi import create_proxy
+from pyscript.ffi import create_proxy
 from pyscript import window, document, PyWorker
 
-from libthree import get_scene, get_camera, get_renderer
-from libthree import get_controls, get_lights
-from libthree import generate_cubes, get_stats
+from pyscript_3dapp_lib.utils import create_hidden_link, rgb_falsecolors, convertxyz2zxy, get_bytes_from_file, write_csv_web
+from pyscript_3dapp_lib.libthree import get_scene, get_camera, get_renderer, get_orbit_ctrl, get_lights, create_grp, create_cube, viz_pts_color, create_sphere, viz_vox_outlines, create_lines
 
-from plyfile import PlyData
-
-async def get_bytes_from_file(item) -> bytes:
-    array_buf = await item.arrayBuffer()
-    return array_buf.to_bytes()
-
-async def read_ply_file(item) -> PlyData:
-    array_buf = await item.arrayBuffer()
-    array_bytes = array_buf.to_bytes()
-    bstream = io.BytesIO(array_bytes)
-    # plydata = PlyData.read(bstream)
-    print('right before plyread')
-    plydata = await asyncio.to_thread(PlyData.read, bstream)
-    return plydata
-
-async def on_pts_submit(e):
-    try:
-        output_p = document.querySelector("#pts-output")
-        output_p.textContent = 'Reading ...'
-        loading_dialog = document.getElementById("loading")
-        loading_dialog.showModal()
-        worker = PyWorker("./worker.py", type="pyodide", config = { "packages": ["plyfile>=1.1.3"] })
-        print("before ready")
-        # Await for the worker
-        await worker.ready
-        print("after ready")
-        file_input = document.querySelector("#pts-file-upload")
-        file_list = file_input.files
-        item = file_list.item(0)
-        my_bytes: bytes = await get_bytes_from_file(item)
-        plydata = await worker.sync.read_ply(my_bytes)
-        # print(len(plydata))
-        output_p.textContent = 'Success'
-        worker.terminate()
-        loading_dialog.close()
-        
-    except Exception as e:
-        print(e)
-        
-scene = get_scene()
-camera = get_camera()
+MRT_RES = None
+PLY_NAME = None
+PLY_PTS = None
+PLY_TEMPS = None
+MN_TEMP = None
+MX_TEMP = None
+VIZ_PTS_MODE = 0
+GRID_PTS = None
+GRID_RAYS = None
+GRID_MS_RAYS = None
+RAYS_ON = None
+height_3dview_ratio = 0.8
+#-----------------------------------------------------------
+# region: get the renderer and append it to index.html
 renderer = get_renderer()
-controls = get_controls(camera, renderer)
+renderer.setSize(window.innerWidth, window.innerHeight*height_3dview_ratio)
+bottom_side = document.getElementById('bottomSide')
+bottom_side.appendChild(renderer.domElement)
+# endregion: get the renderer and append it to index.html
+#-----------------------------------------------------------
+# region setup scene and cam
+# get the camera and scene
+camera = get_camera()
+# get the scene
+scene = get_scene()
+# get lights and put in the scene
 lights = get_lights()
-# stats = get_stats()
-cubes = []
+for light in lights:
+    scene.add(light)
 
-def init():
-    for cube in generate_cubes(scale=1000):
-        scene.add(cube)
-        cubes.append(cube)
-        if len(cubes) == 50000:
-            break
+# disable the buttons
+dl_btn = document.getElementById("mrt-download")
+dl_btn.disabled = True
 
-    for light in lights:
-        scene.add(light)
+vizpts_btn = document.getElementById("viz_pts")
+vizpts_btn.disabled = True
 
-def animate(now):
-    # stats.begin()
+gridid_btn = document.getElementById("grid_id")
+gridid_btn.disabled = True
+
+vizrays_btn = document.getElementById("viz_rays")
+vizrays_btn.disabled = True
+
+# orbit controls
+controls = get_orbit_ctrl(camera, renderer)
+# create a spinning cube
+init_cube, init_edges = create_cube()
+init_edges.name = 'init_edges'
+scene.add(init_edges)
+
+def set_cam_orig():
+    camera.position.set(1, 1, 4)
+    camera.lookAt(0,0,0)
+
+set_cam_orig()
+
+def animate(*args):
     controls.update()
-    camera.position.x += math.sin(now * 0.0001)
-    camera.position.z += math.cos(now * 0.0001)
-    light_back_green, light_back_white = lights
-    light_back_green.position.x = camera.position.x
-    light_back_green.position.y = camera.position.y
-    light_back_green.position.z = camera.position.z
-    light_back_white.position.x = camera.position.x
-    light_back_white.position.y = camera.position.y
-    light_back_white.position.z = camera.position.z
-    camera.lookAt(scene.position)
+    init_edges.rotation.x += 0.01
+    init_edges.rotation.y += 0.01
     renderer.render(scene, camera)
-    # stats.end()
-    window.requestAnimationFrame(animate_js)
+    # Call the animation loop recursively
+    window.requestAnimationFrame(animate_proxy)
 
-animate_js = create_proxy(animate)
+animate_proxy = create_proxy(animate)
+window.requestAnimationFrame(animate_proxy)
+# endregion: setup scene and cam
+#-----------------------------------------------------------
+
+def change_dialog_text(txt: str):
+    text_elem = document.getElementById("dialogText")
+    text_elem.innerText = txt
+
+def change_color_bar(mnval: float, mxval: float):
+    val_range = mxval - mnval
+    interval = val_range/4
+    intervals = [mnval, mnval+interval, mnval+(interval*2), mnval+(interval*3), mxval]
+    for cnt, i in enumerate(intervals):
+        color_label = document.getElementById("fcval" + str(cnt+1))
+        color_label.textContent = str(round(i, 1))
+
+def grid_pts_mrt2rows(grid_pts: list[list[float]], mrts: list[float]) -> list[list]:
+    """
+    convert grid pts and mrts to rows
+
+    Parameters
+    ----------
+    grid_pts: list[list[float]]
+        mrt gridpts
+    
+    mrts: list[float]
+        mrts calculated corresponding to the grid pts
+
+    Returns
+    -------
+    list[list]
+        list[shape(npts, 4)]  
+    """
+    header_str = ['x', 'y', 'z', 'MRT(degC)']
+    rows = [header_str]
+    for cnt,grid_pt in enumerate(grid_pts):
+        row = [grid_pt[0], grid_pt[1], grid_pt[2], mrts[cnt]]
+        rows.append(row)
+    return rows
+
+def viz_a_grid_rays(grid_pt: list[float], grid_rays: list[list[float]], rgb: list[float]):
+    """
+    process and viz rays of a grid point
+
+    Parameters
+    ----------
+    grid_pt: list[float]
+        list[shape(3)] xyz of the point
+    
+    grid_rays: list[list[float]]
+        list[shape(nrays, 3)] intersections rays with bbox
+
+    grid_id: int
+        id of the grid to viz
+
+    Returns
+    -------
+    THREE.LineSegments
+        threejs lines that can be visualize
+    """
+    nrays = len(grid_rays)
+    grid_pts = np.repeat([grid_pt], nrays, axis=0)
+    grid_rays = np.array(grid_rays)
+    grid_rays = convertxyz2zxy(grid_rays)
+    lines_xyzs = np.hstack((grid_pts, grid_rays))
+    lines_xyzs_flat = lines_xyzs.flatten().tolist()
+    threejs_lines = create_lines(lines_xyzs_flat, rgb_color=rgb)
+    return threejs_lines
+    
+async def on_submit(e):
+    try:
+        # region: get all the parameters
+        t1 = perf_counter()
+        submit_btn = document.getElementById("stcsv-submit")
+        submit_btn.disabled = True
+        set_cam_orig()
+        init_edges = scene.getObjectByName( "init_edges", True )
+        scene.add(init_edges)
+        output_p = document.querySelector("#stcsv-output")
+        output_p.textContent = 'Calculating ...'
+        st_file_input = document.querySelector("#stpts-file-upload")
+        grid_file_input = document.querySelector("#grid-file-upload")
+        st_file_list = st_file_input.files
+        grid_file_list = grid_file_input.files
+        nstfiles = len(st_file_list)
+        ngfiles = len(grid_file_list)
+        # endregion: get all the parameters
+        if nstfiles != 0 and ngfiles != 0:
+            # region: loading dialog and get extra parameters
+            loading_dialog = document.getElementById("loading")
+            loading_dialog.showModal()
+            st_item = st_file_list.item(0)
+            global PLY_NAME
+            st_full_name = st_item.name
+            st_name = st_full_name.split('.')[0]
+            PLY_NAME = st_name
+            grid_item = grid_file_list.item(0)
+            st_bytes = await get_bytes_from_file(st_item)
+            grid_bytes = await get_bytes_from_file(grid_item)
+            vdim = float(document.querySelector("#vdim").value)
+            nrays = float(document.querySelector("#nray").value)
+            # Await for the worker
+            worker_config = {
+                                "packages": ["plyfile>=1.1.3", "geomie3d==0.0.11", "numpy-stl>=3.2.0",
+                                            "./lib/pyscript_3dapp_lib-0.0.2-py3-none-any.whl", 
+                                            "./lib/raytrace_mrt_lib-0.0.2-py3-none-any.whl"]
+                            }
+            worker = PyWorker("./worker.py", type="pyodide", config = worker_config)
+            # Await for the worker
+            await worker.ready
+            worker.sync.change_dialog_text = change_dialog_text
+            # endregion: loading dialog and get extra parameters
+            # region: calculate the mrt and retrieve information from the calc
+            world = create_grp()
+            mrt_data = await worker.sync.calc_mrt(st_bytes, grid_bytes, vdim, nrays)
+            vx_midpts = list(mrt_data.midpts)
+            vx_midpts = list(map(list, vx_midpts))
+            nmidpts = len(vx_midpts)
+            vx_temps = list(mrt_data.temps)
+            cam_pos = mrt_data.cam[0]
+            lookat = mrt_data.cam[1]
+            global GRID_PTS
+            grid_pts = mrt_data.grid
+            GRID_PTS = grid_pts
+            mrt_ls = mrt_data.mrt
+            mrt_ls = np.round(mrt_ls,2).tolist()
+            global PLY_PTS
+            pts = list(mrt_data.pts)
+            PLY_PTS = pts
+            global PLY_TEMPS
+            pts_temps = list(mrt_data.pts_temp)
+            PLY_TEMPS = pts_temps
+            global GRID_RAYS
+            grid_rays = mrt_data.rays # shape(ngrids, nintx, 3)
+            GRID_RAYS = grid_rays
+            global GRID_MS_RAYS
+            grid_ms_rays = mrt_data.miss_rays # shape(ngrids, nmiss, 3)
+            GRID_MS_RAYS = grid_ms_rays
+            # endregion: calculate the mrt and retrieve information from the calc
+            # region: convert all temps to falsecolor
+            mn_vxtemp = min(vx_temps)
+            mx_vxtemp = max(vx_temps)
+            mn_mrt = min(mrt_ls)
+            mx_mrt = max(mrt_ls)
+            mn_temp = min([mn_vxtemp, mn_mrt])
+            mx_temp = max([mx_vxtemp, mx_mrt])
+            global MN_TEMP
+            MN_TEMP = mn_temp
+            global MX_TEMP
+            MX_TEMP = mx_temp
+            change_color_bar(mn_temp, mx_temp)
+            nvx_verts = 24
+            vx_temps2 = np.repeat(vx_temps, nvx_verts)
+            vx_colors = rgb_falsecolors(vx_temps2, mn_temp, mx_temp)
+            vx_colors = np.reshape(vx_colors, (nmidpts, nvx_verts*3)).tolist()
+            # endregion: convert all temps to falsecolor
+            # region: viz voxels mrt to falsecolor and viz the points
+            outlines = viz_vox_outlines(vx_midpts, vx_colors, vdim)
+            world.add(outlines)
+            
+            mrt_colors = rgb_falsecolors(mrt_ls, mn_temp, mx_temp)
+            mrt_colors = np.reshape(mrt_colors, (len(mrt_ls), 3)).tolist()
+            for gcnt, grid_pt in enumerate(grid_pts):
+                rgb = mrt_colors[gcnt]
+                grid_sphere = create_sphere(0.1, 10, 10, r = rgb[0], g = rgb[1], b = rgb[2])
+                grid_sphere.position.x = grid_pt[0]
+                grid_sphere.position.y = grid_pt[1]
+                grid_sphere.position.z = grid_pt[2]
+                world.add(grid_sphere)
+            # endregion: viz voxels mrt to falsecolor and viz the points
+            # region: prepare the 3d scene
+            camera.position.set(cam_pos[0], cam_pos[1], cam_pos[2])
+            camera.lookAt(lookat)
+
+            scene.remove(init_edges)
+            scene.add(world)
+            worker.terminate()
+            loading_dialog.close()
+            # endregion: prepare the 3d scene
+            # region: prepare data for downloads and other viz
+            global MRT_RES
+            csv_rows = grid_pts_mrt2rows(grid_pts, mrt_ls)
+            mrt_res = write_csv_web(csv_rows)
+            MRT_RES = mrt_res
+            t2 = perf_counter()
+            dur = int((t2 - t1)/60)
+            if dur == 0:
+                dur = 'less than a minute'
+            output_p.textContent = f"Success! Time Elapsed (mins): {dur}"
+            
+            dl_btn.disabled = False
+            dl_msg = document.querySelector("#mrt-output")
+            dl_msg.textContent = 'Refresh to do another projection.'
+
+            vizpts_btn.disabled = False
+            gridid_btn.disabled = False
+            vizrays_btn.disabled = False
+            # endregion: prepare data for downloads and other viz
+        else:
+            loading_dialog = document.getElementById("loading")
+            loading_dialog.showModal()
+            change_dialog_text('Please specify PLY and CSV file')
+            
+    except Exception as e:
+        change_dialog_text(e)
+        print(e)
+
+def downloadFile(*args):
+    create_hidden_link(MRT_RES, f"{PLY_NAME}_mrt_res", 'csv')
+
+def viz_pts(*args):
+    global VIZ_PTS_MODE
+    if VIZ_PTS_MODE == 0:
+        pts_colors = rgb_falsecolors(PLY_TEMPS, MN_TEMP, MX_TEMP)
+        three_js_pts = viz_pts_color(PLY_PTS, pts_colors, size=0.05)
+        three_js_pts.name = 'three_js_pts'
+        scene.add(three_js_pts)
+        VIZ_PTS_MODE = 1
+    else:
+        three_js_pts = scene.getObjectByName( "three_js_pts", True )
+        scene.remove(three_js_pts)
+        VIZ_PTS_MODE = 0
+
+def viz_rays(*args):
+    global RAYS_ON
+    grid_pts = GRID_PTS
+    rays = GRID_RAYS
+    ms_rays = GRID_MS_RAYS
+    grid_id = int(document.querySelector("#grid_id").value)
+    grid_pt = grid_pts[grid_id-1]
+    grid_rays = rays[grid_id-1]
+    grid_ms_rays = ms_rays[grid_id-1]
+    if RAYS_ON == None:
+        threejs_lines = viz_a_grid_rays(grid_pt, grid_rays, [1,0,0])
+        threejs_lines.name = f"grid_rays{grid_id}"
+        three_js_lines_ms = viz_a_grid_rays(grid_pt, grid_ms_rays, [1,1,1])
+        three_js_lines_ms.name = f"grid_ms_rays{grid_id}"
+        RAYS_ON = grid_id
+        scene.add(threejs_lines)
+        scene.add(three_js_lines_ms)
+    else:
+        three_js_lines = scene.getObjectByName(f"grid_rays{RAYS_ON}", True)
+        three_js_lines_ms = scene.getObjectByName( f"grid_ms_rays{RAYS_ON}", True)
+        scene.remove(three_js_lines)
+        scene.remove(three_js_lines_ms)
+        if grid_id != RAYS_ON:
+            threejs_lines = viz_a_grid_rays(grid_pt, grid_rays, [1,0,0])
+            threejs_lines.name = f"grid_rays{grid_id}"
+            three_js_lines_ms = viz_a_grid_rays(grid_pt, grid_ms_rays, [1,1,1])
+            three_js_lines_ms.name = f"grid_ms_rays{grid_id}"
+            RAYS_ON = grid_id
+            scene.add(threejs_lines)
+            scene.add(three_js_lines_ms)
+        else:
+            RAYS_ON = None
 
 if __name__ == "__main__":
-    init()
-    animate(time.time())
-    add_event_listener(document.getElementById("pts-submit"), "click", lambda e: asyncio.create_task(on_pts_submit(e)) )
+    animate()
+    add_event_listener(document.getElementById("stcsv-submit"), "click", lambda e: asyncio.create_task(on_submit(e)))
+    add_event_listener(document.getElementById("mrt-download"), "click", downloadFile)
+    add_event_listener(document.getElementById("viz_pts"), "click", viz_pts)
+    add_event_listener(document.getElementById("viz_rays"), "click", viz_rays)
--- raytracemrt_webapp/worker.py
+++ raytracemrt_webapp/worker.py
@@ -1,20 +1,215 @@
 import io
-from pyscript import sync
-from plyfile import PlyData
+import csv
+
+import geomie3d
 import numpy as np
 
-def heavy_compute():
-    res = np.array([1234, 5678, 91011])
-    return res
+from raytrace_mrt_lib import separate_rays, gen_rays
+from pyscript_3dapp_lib.utils import read_csv_web, convertxyz2zxy, read_ply_web, get_cam_place_from_xyzs
+from pyscript import sync
 
-sync.heavy_compute = heavy_compute
+def process_grid_data(csv_rows: list[list]) -> list[list]:
+    """
+    read csv file for webapp
 
-def read_ply(ply_bytes: bytes):
-    ply_bytes = ply_bytes.to_py()
-    bstream = io.BytesIO(ply_bytes)
-    plydata = PlyData.read(bstream)
-    vertices = plydata['vertex'].data.tolist()
-    xyzs_ls = np.array(vertices)
-    return xyzs_ls
+    Parameters
+    ----------
+    csv_rows: list[list]
+        list[shape(nrows, ncolumns)].
 
-sync.read_ply = read_ply
+    Returns
+    -------
+    list[list]
+        list[shape(ngrid_pts, 3)]
+    """
+    pts = csv_rows[1:]
+    pts_arr = np.array(pts)
+    pts_arr = pts_arr.astype(float).tolist()
+    return pts_arr
+    
+def process_plydata(plydata: np.ndarray) -> dict:
+    """
+    separate the plydata into xyzs and temperatures
+
+    Parameters
+    ----------
+    plydata: np.ndarray
+        np.ndarray[shape(n_pts, n_attributes)] 
+
+    Returns
+    -------
+    dict
+        A dictionary containing:
+            - "xyzs": np.ndarray[(n_pts, 3)].
+            - "temps": np.ndarray[(n_pts)].
+    """
+    temps = plydata[:, 3]
+    vertices = plydata[:, 0:3]
+    return {'xyzs': vertices, 'temps': temps}
+
+def calc_mrt(ply_bytes: bytes, grid_bytes: bytes, vdim: float, nrays: int) -> dict:
+    """
+    calc mrt
+
+    Parameters
+    ----------
+    ply_bytes: bytes
+        JS bytes from the file specified. Need to be converted to python with .to_py() function.
+
+    grid_bytes: bytes
+        JS bytes from the file specified. Need to be converted to python with .to_py() function.
+
+    vdim: float
+        dimesion of a voxel in meters.
+
+    nrays: int
+        number of rays to cast per grid point.
+        
+    Returns
+    -------
+    flatten_mesh_xyzs : np.ndarray
+        np.ndarray[shape(ntri * 3 * 3)]
+    """
+    #------------------------------------------------------------------
+    # region: read ply file
+    sync.change_dialog_text('Reading PLY file ...')
+    plydata = read_ply_web(ply_bytes)
+    plydata = process_plydata(plydata)
+    ply_xyzs = plydata['xyzs']
+    ply_temps = plydata['temps']
+    # endregion: read ply file
+    #------------------------------------------------------------------
+    # region: convert ply pts to voxels
+    sync.change_dialog_text('Convert PLY pts to voxels ...')
+    vxres_dict = geomie3d.modify.xyzs2voxs(ply_xyzs, vdim, vdim, vdim)
+    #convert the voxels to bboxes
+    vxs = vxres_dict['voxels']
+    vx_dim = vxres_dict['voxel_dim']
+    ijks = vxs.keys()
+    nijk = len(ijks)
+    midpts = []
+    avg_temps = []
+    atts = []
+    xdims = np.array([vx_dim[0]])
+    xdims = np.repeat(xdims, nijk)
+    ydims = np.array([vx_dim[1]])
+    ydims = np.repeat(ydims, nijk)
+    zdims = np.array([vx_dim[2]])
+    zdims = np.repeat(zdims, nijk)
+    sync.change_dialog_text('Convert voxels to bounding boxes ...')
+    for ijk in ijks:
+        vx = vxs[ijk]
+        midpt = vx['midpt']
+        midpt = list(map(float, midpt))
+        idxs = vx['idx']
+        sel_temps = np.take(ply_temps, idxs)
+        avg_temp = float(np.mean(sel_temps))
+        att = {'idx': vx['idx'], 'ijk': ijk, 'midpt':midpt, 'temperature': avg_temp}
+        avg_temps.append(avg_temp)
+        midpts.append(midpt)
+        atts.append(att)
+    bbx_ls = geomie3d.create.bboxes_frm_midpts(midpts, xdims, ydims, zdims, attributes_list = atts)
+    # endregion: read ply file
+    #------------------------------------------------------------------
+    # region: read csv file and convert them into rays for projection
+    sync.change_dialog_text('Reading CSV file and generating rays ...')
+    csv_rows = read_csv_web(grid_bytes)
+    grid_pts = process_grid_data(csv_rows)
+    ngrids = len(grid_pts)
+    rays = gen_rays(grid_pts, nrays)
+
+    aloop = 1000000#30
+    nbbox = len(bbx_ls)
+    ndir = len(rays)
+    ttl = ndir*nbbox
+    nparallel = int(ttl/aloop)
+    
+    if nparallel != 0:
+        rays_ls = separate_rays(rays, nparallel)
+    # endregion: read csv file and convert them into rays for projection
+    #------------------------------------------------------------------
+    # region: project the rays onto the bboxes
+    sync.change_dialog_text('Projecting ray onto bboxes ...')
+    ttl_k = int(ttl/1000)
+    proj_rays = []
+    ms_rays = []
+    rcnt = 0
+    for rays1 in rays_ls:
+        rcnt += len(rays1)
+        percentage = int((rcnt*nbbox)/ttl * 100)
+        hrs, mrs, hbs, mbs = geomie3d.calculate.rays_bboxes_intersect(rays1, bbx_ls)
+        nhr = len(hrs)
+        nmr = len(mrs)
+        msg = f"Projecting {ndir} ray onto {nbbox} Voxels ... \n{percentage}% of {ttl_k}k calculations completed"
+        msg += f"\n{nhr} rays intersection, {nmr} rays did not hit any voxels"
+        sync.change_dialog_text(msg)
+        proj_rays.extend(hrs)
+        ms_rays.extend(mrs)
+    # endregion: project the rays onto the bboxes
+    #------------------------------------------------------------------
+    # region: process the raytracing results
+    grid_temps = []
+    for _ in range(ngrids):
+        grid_temps.append([])
+
+    grid_intxs = []
+    for _ in range(ngrids):
+        grid_intxs.append([])
+
+    grid_ms_rays = []
+    for _ in range(ngrids):
+        grid_ms_rays.append([])
+    
+    for proj_ray in proj_rays:
+        grid_id = proj_ray.attributes['grid_id']
+        intx_att = proj_ray.attributes['rays_bboxes_intersection']
+        hit_bbxs = intx_att['hit_bbox']
+        if len(hit_bbxs) == 1:
+            if 'temperature' in hit_bbxs[0].attributes:
+                intxs = intx_att['intersection']
+                temp = hit_bbxs[0].attributes['temperature']
+                grid_temps[grid_id].append(temp)
+                grid_intxs[grid_id].extend(intxs)
+        else:
+            ijks = [hb.attributes['ijk'] for hb in hit_bbxs]
+            unq = np.unique(ijks, axis=0)
+            if len(unq) == 1:
+                intxs = intx_att['intersection']
+                temp = hit_bbxs[0].attributes['temperature']
+                grid_temps[grid_id].append(temp)
+                grid_intxs[grid_id].extend(intxs)
+            else:
+                print('not sure what is happening please debug')
+
+    for ms_ray in ms_rays:
+        grid_id = ms_ray.attributes['grid_id']
+        orig = ms_ray.origin
+        dirx = ms_ray.dirx
+        mv_xyzs = geomie3d.calculate.move_xyzs([orig], [dirx], [5])
+        grid_ms_rays[grid_id].extend(mv_xyzs)
+
+    mrt_ls = []
+    for gcnt,gt in enumerate(grid_temps):
+        if len(gt) != 0:
+            avg = sum(gt)/len(gt)
+            mrt_ls.append(avg)
+        else:
+            print(f"grid pt {gcnt} do not see any temperatures")
+            mrt_ls.append(-999)
+
+    # endregion: process the raytracing results
+    #------------------------------------------------------------------
+    # region: prepare data to return to main script
+    cam_place = get_cam_place_from_xyzs(midpts, zoom_out_val = 5)
+    cam_place_zxy = convertxyz2zxy(cam_place) 
+    midpts_zxy = convertxyz2zxy(midpts)
+    grid_pts_zxy = convertxyz2zxy(grid_pts)
+    ply_zxy = convertxyz2zxy(ply_xyzs)
+    ply_zxy = ply_zxy.flatten()
+    # grid_intxs_zxy = convertxyz2zxy(grid_intxs)
+    return {'midpts': midpts_zxy, 'temps': avg_temps, 'mrt': mrt_ls, 'cam': cam_place_zxy, 'grid': grid_pts_zxy, 'pts': ply_zxy, 
+            'pts_temp': ply_temps, 'rays': grid_intxs, 'miss_rays': grid_ms_rays}
+    # endregion: prepare data to return to main script
+    #------------------------------------------------------------------
+
+sync.calc_mrt = calc_mrt
